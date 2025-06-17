7015ceeb87aee51777ccfcd93ae12c80015d809f
hammerklavier <q5vsx3@163.com>
2025-06-17 18:18:39 +0800

    use cratees/slint-interface/src/main.rs as launcher for slint-interface instead of crates/launcher/src/main.rs

---
+++ crates/slint-interface/src/main.rs
@@ -0,0 +1,42 @@
+#![windows_subsystem = "windows"]
+use log;
+mod slint_interface;
+
+const DEFAULT_INTERFACE: &str = "TRANSLATOR_INTERFACE";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello, world!");
+
+    unsafe {
+        let rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
+        if vec!["trace", "debug", "info", "warn", "error"]
+            .contains(&rust_log.to_lowercase().as_str())
+        {
+            std::env::set_var("RUST_LOG", rust_log);
+        } else {
+            std::env::set_var("RUST_LOG", "info");
+        }
+    }
+
+    env_logger::init();
+    log::info!(
+        "Logging level: {}",
+        std::env::var("RUST_LOG").unwrap_or_else(|_| "warn".to_string())
+    );
+    log::info!(
+        "Designated interface: {}",
+        std::env::var(DEFAULT_INTERFACE).unwrap_or_else(|_| "slint".to_string())
+    );
+
+    // if let Some(err) = backends::initialise().err() {
+    //     match err.downcast_ref::<backends::error::Error>() {
+    //         // TODO
+    //         Some(backends::error::Error::ConfigFileBadFormat(path, s)) => {
+    //             log::error!("No backend found");
+    //         }
+    //         _ => {}
+    //     }
+    // };
+
+    Ok(slint_interface::run()?)
+}
